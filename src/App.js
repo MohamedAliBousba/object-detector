48a4c8e45c769ffd5b7efaca35d53098e0069618
Mohamed Ali <im_bousba@esi.dz>
2022-03-13 14:59:36 +0100

    add object detector

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components"
+import ObjectDetector from './ObjectDetector';
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: #1c2127;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <ObjectDetector />
+    </AppContainer>
   );
 }
 
